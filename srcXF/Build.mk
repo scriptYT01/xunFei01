998889dfb6d7023b89f348b29c8ed1e0d8d960ab
yt01 <yt01@jjj123.com>
2017-07-19 17:03:03 +0800

    _2017_0719__1703pm

--- srcXF/Build.mk
+++ srcXF/Build.mk
@@ -1,20 +1,22 @@
 LOCAL_PATH := $(my-dir)
 
-#==== ym_hood_wifi ==========================================
+#==== tts_sample ==========================================
 include $(CLEAR_VARS)
-LOCAL_MODULE := ym_hood_wifi
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := tts_sample.bin
+LOCAL_MODULE_TAGS := tts_sample
 LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/$(TARGET_TESTSUIT_DIR)/network
-LOCAL_SRC_FILES := ym_hood_wifi.c
-LOCAL_LDLIBS := -lc -lpthread
+LOCAL_SRC_FILES := samples/tts_sample/tts_sample.c
+LOCAL_LDLIBS := -lmsc -lrt -ldl -lpthread 
 include $(BUILD_EXECUTABLE)
+
 #==== miio_wifi ==========================================
-LOCAL_MODULE := miio_wifi
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/$(TARGET_TESTSUIT_DIR)/network
-LOCAL_SRC_FILES := miio_wifi.c cJSON.c data_Analysis.c muc_stm32.c         
-########LOCAL_CFLAGS := -Wall -O -Werror -DVERSION=\"0.1.4\"
-LOCAL_CFLAGS := -Wall -O -DVERSION=\"0.1.4\"
-LOCAL_LDLIBS := -lc -lpthread -lm
-include $(BUILD_EXECUTABLE)
+#LOCAL_MODULE := miio_wifi
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/$(TARGET_TESTSUIT_DIR)/network
+#LOCAL_SRC_FILES := miio_wifi.c cJSON.c data_Analysis.c muc_stm32.c         
+#########LOCAL_CFLAGS := -Wall -O -Werror -DVERSION=\"0.1.4\"
+#LOCAL_CFLAGS := -Wall -O -DVERSION=\"0.1.4\"
+#LOCAL_LDLIBS := -lc -lpthread
+#LOCAL_LDLIBS := -lc -lpthread -lm
+#include $(BUILD_EXECUTABLE)
 
