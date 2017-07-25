93c213c5253f504f565d14df718fc394011b1965
yt01 <yt01@jjj123.com>
2017-07-25 12:15:53 +0800

    /home/dyn_ym/dyn/viomi/v101.xunfei/_2017_0725__1215pm

--- srcXF10/Build.mk
+++ srcXF10/Build.mk
@@ -1,28 +1,50 @@
 LOCAL_PATH := $(my-dir)
 
-#==== tts_sample ==========================================
+#==== tts_sample11 ==========================================
 include $(CLEAR_VARS)
 LOCAL_MODULE := tts_sample11.bin
-LOCAL_MODULE_TAGS := tts_sample
+LOCAL_MODULE_TAGS := tts_sample11
 LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/$(TARGET_TESTSUIT_DIR)/network
 LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/ch/xf10
 LOCAL_SRC_FILES := samples/tts_sample/tts_sample.c
 #LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc -lrt -ldl -lpthread -lm -lc
-#LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc -lrt -ldl -lpthread -lc 
-#LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc -lrt -ldl -lc 
-#LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc -ldl -lc 
 LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc -lc 
 #LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc 
 include $(BUILD_EXECUTABLE)
 
-#==== miio_wifi ==========================================
-#LOCAL_MODULE := miio_wifi
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/$(TARGET_TESTSUIT_DIR)/network
-#LOCAL_SRC_FILES := miio_wifi.c cJSON.c data_Analysis.c muc_stm32.c         
-#########LOCAL_CFLAGS := -Wall -O -Werror -DVERSION=\"0.1.4\"
-#LOCAL_CFLAGS := -Wall -O -DVERSION=\"0.1.4\"
-#LOCAL_LDLIBS := -lc -lpthread
-#LOCAL_LDLIBS := -lc -lpthread -lm
-#include $(BUILD_EXECUTABLE)
+#==== tts_tcp12 ==========================================
+include $(CLEAR_VARS)
+LOCAL_MODULE := tts_tcp12.bin
+LOCAL_MODULE_TAGS := tts_tcp12
+LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/$(TARGET_TESTSUIT_DIR)/network
+LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/ch/xf10
+LOCAL_SRC_FILES := samples/tts_sample/tts_tcp.c
+#LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc -lrt -ldl -lpthread -lm -lc
+LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc -lc 
+#LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc 
+include $(BUILD_EXECUTABLE)
+
+#==== asr_sample13 ==========================================
+include $(CLEAR_VARS)
+LOCAL_MODULE := asr_sample13.bin
+LOCAL_MODULE_TAGS := asr_sample13
+LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/$(TARGET_TESTSUIT_DIR)/network
+LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/ch/xf10
+LOCAL_SRC_FILES := samples/asr_sample/asr_sample.c
+#LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc -lrt -ldl -lpthread -lm -lc
+LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc -lc 
+#LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc 
+include $(BUILD_EXECUTABLE)
+
+#==== asr_tcp14 ==========================================
+include $(CLEAR_VARS)
+LOCAL_MODULE := asr_tcp14.bin
+LOCAL_MODULE_TAGS := asr_tcp14
+LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/$(TARGET_TESTSUIT_DIR)/network
+LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/ch/xf10
+LOCAL_SRC_FILES := samples/asr_sample/asr_tcp.c
+#LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc -lrt -ldl -lpthread -lm -lc
+LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc -lc 
+#LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc 
+include $(BUILD_EXECUTABLE)
 
