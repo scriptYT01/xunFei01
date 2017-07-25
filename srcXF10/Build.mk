e1dc0bfaea40cc16a9220ef01365bc109707dc55
yt01 <yt01@jjj123.com>
2017-07-25 14:28:37 +0800

    /home/dyn_ym/dyn/viomi/v101._2017_0725__1428pm

--- srcXF10/Build.mk
+++ srcXF10/Build.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(my-dir)
 
 #==== tts_sample11 ==========================================
 include $(CLEAR_VARS)
-LOCAL_MODULE := tts_sample11.bin
+LOCAL_MODULE := tts_sample11.x1000.bin
 LOCAL_MODULE_TAGS := tts_sample11
 LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/$(TARGET_TESTSUIT_DIR)/network
 LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/ch/xf10
@@ -14,7 +14,7 @@ include $(BUILD_EXECUTABLE)
 
 #==== tts_tcp12 ==========================================
 include $(CLEAR_VARS)
-LOCAL_MODULE := tts_tcp12.bin
+LOCAL_MODULE := tts_tcp12.x1000.bin
 LOCAL_MODULE_TAGS := tts_tcp12
 LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/$(TARGET_TESTSUIT_DIR)/network
 LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/ch/xf10
@@ -26,7 +26,7 @@ include $(BUILD_EXECUTABLE)
 
 #==== asr_sample13 ==========================================
 include $(CLEAR_VARS)
-LOCAL_MODULE := asr_sample13.bin
+LOCAL_MODULE := asr_sample13.x1000.bin
 LOCAL_MODULE_TAGS := asr_sample13
 LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/$(TARGET_TESTSUIT_DIR)/network
 LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/ch/xf10
@@ -38,7 +38,7 @@ include $(BUILD_EXECUTABLE)
 
 #==== asr_tcp14 ==========================================
 include $(CLEAR_VARS)
-LOCAL_MODULE := asr_tcp14.bin
+LOCAL_MODULE := asr_tcp14.x1000.bin
 LOCAL_MODULE_TAGS := asr_tcp14
 LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/$(TARGET_TESTSUIT_DIR)/network
 LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/ch/xf10
--- srcXF20/Build.mk
+++ srcXF20/Build.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(my-dir)
 
 #==== tts_sample ==========================================
 include $(CLEAR_VARS)
-LOCAL_MODULE := tts_sample21.bin
+LOCAL_MODULE := tts_sample21.x1000.bin
 LOCAL_MODULE_TAGS := tts_sample
 LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/$(TARGET_TESTSUIT_DIR)/network
 LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/ch/xf20
