361c7d9997162791105cdc4fcf851994113ccb36
yt01 <yt01@jjj123.com>
2017-08-15 15:21:56 +0800

    _2017_0815__1521pm

--- srcXF30/dbmem.c
+++ srcXF30/dbmem.c
@@ -1,32 +1,56 @@
 
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <dlfcn.h>
 
 // LD_PRELOAD=/ch/xf30/dbmem.so ./aiui_sample32.x1000.bi
 
 int _myDebug01 = 0 ;
 
+//FILE *(*original_fopen)(const char*, const char*);
+//original_fopen = dlsym(RTLD_NEXT, "fopen");
+//return (*original_fopen)(path, mode);
+
+void * (*_origin__malloc)(size_t ) = NULL ;
+void (*_origin__free)(void *) = NULL ;
+
 static void show_dbinfo_once( char ___who ){
     _myDebug01 ++ ;
     switch ( _myDebug01 ) {
-        case 0 :
         case 1 :
             printf( "\n" "myDebug init : %c" "\n" , ___who ) ;
             break ;
         default:
             break ;
     }
+    if ( NULL == _origin__malloc ) {
+        _origin__malloc     = dlsym( RTLD_NEXT , "malloc" ) ;
+        _origin__free       = dlsym( RTLD_NEXT , "free" ) ;
+        if ( NULL == _origin__malloc || NULL == _origin__free ) {
+            printf( "\n" " can not get func. " " %p %p , exit.\n" , _origin__malloc , _origin__free);
+            exit( 21 ) ;
+        }
+        if ( _origin__free == _origin__malloc ) {
+            printf( "\n" " why equal . " " %p %p , exit.\n" , _origin__malloc , _origin__free);
+            exit( 22 ) ;
+        }
+        printf( "\n" " malloc , free : " " %p %p , exit.\n" , _origin__malloc , _origin__free);
+    }
 } // show_dbinfo_once
 
 void *malloc(size_t ___size) {
+    void * __v01 ;
     show_dbinfo_once( 'm' ) ;
 
-    return malloc( ___size ) ;
+    __v01 = _origin__malloc( ___size ) ;
+    return __v01 ;
 } // malloc
 
 void free(void *___ptr){
     show_dbinfo_once( 'f' ) ;
 
-    free( ___ptr );
+    _origin__free( ___ptr );
 } // free
 
