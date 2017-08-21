79cb41fb1f7ae98513eabfade696fdea795f18f6
yt01 <yt01@jjj123.com>
2017-08-21 15:24:05 +0800

    _2017_0821__1524pm

--- srcXF40/alsa-record-example41.c
+++ srcXF40/alsa-record-example41.c
@@ -22,7 +22,8 @@
 #define _prSF( fmt , ... ) printf( "--debuging: %s %d %s : " fmt , basename(__FILE__), __LINE__, __func__ , ## __VA_ARGS__ )
 #define _prSFn( fmt , ... ) _prSF( fmt "\n" , ## __VA_ARGS__ )
 	      
-char * _dev_name01 = "default" ;
+char *  _dev_name01 = "default" ;
+int     _chAmount = 2 ;
 
 int main (int ___argc, char *___argv[])
 {
@@ -30,7 +31,8 @@ int main (int ___argc, char *___argv[])
     int err;
     char *buffer;
     int buffer_frames = 128;
-    unsigned int rate = 44100;
+    //unsigned int rate = 44100;
+    unsigned int rate = 16000;
     snd_pcm_t *capture_handle;
     snd_pcm_hw_params_t *hw_params;
     snd_pcm_format_t format = SND_PCM_FORMAT_S16_LE;
@@ -47,93 +49,101 @@ int main (int ___argc, char *___argv[])
             _prSFn( " arg 2 <%s>" , ___argv[2] ) ; 
         }
     }
+    if ( ___argc >=3 && ___argv[2] != NULL && ___argv[2][0] != 0 ) {
+        _chAmount = atoi( ___argv[2] ) ;
+    }
 
-    _prSFn( " trying snd_pcm_open  <%s>" , _dev_name01 ) ; 
+    _prSFn( " ------ trying snd_pcm_open  <%s>" , _dev_name01 ) ; 
 
+    //int snd_pcm_open( snd_pcm_t **handle, int card, int device, int mode );
     if ((err = snd_pcm_open (&capture_handle, _dev_name01, SND_PCM_STREAM_CAPTURE, 0)) < 0) {
-        _prSFn( "cannot open audio device %s (%s)\n", 
+        _prSFn( "cannot open audio device %s (%d:%s)", 
                 _dev_name01,
-                snd_strerror (err));
+                err , snd_strerror (err));
         exit (1);
     }
-    _prSFn( "audio interface opened\n" );
+    _prSFn( "audio interface opened" );
 
 
     if ((err = snd_pcm_hw_params_malloc (&hw_params)) < 0) {
-        _prSFn( "cannot allocate hardware parameter structure (%s)\n",
-                snd_strerror (err));
+        _prSFn( "cannot allocate hardware parameter structure (%d:%s)",
+                err , snd_strerror (err));
         exit (1);
     }
-    _prSFn( "hw_params mallocated succeed.\n" );
+    _prSFn( "hw_params mallocated succeed." );
 
     if ((err = snd_pcm_hw_params_any (capture_handle, hw_params)) < 0) {
-        _prSFn( "cannot initialize hardware parameter structure (%s)\n",
-                snd_strerror (err));
+        _prSFn( "cannot initialize hardware parameter structure (%d:%s)",
+                err , snd_strerror (err));
         exit (1);
     }
-    _prSFn( "hw_params initialized\n" );
+    _prSFn( "hw_params initialized" );
 
     if ((err = snd_pcm_hw_params_set_access (capture_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
-        _prSFn( "cannot set access type (%s)\n",
-                snd_strerror (err));
+        _prSFn( "cannot set access type (%d:%s)",
+                err, snd_strerror (err));
         exit (1);
     }
-    _prSFn( "hw_params access setted\n");
+    _prSFn( "hw_params access setted");
 
+    _prSFn( "trying to set hw_params rate to <%d : %s>" , format , 
+            format==SND_PCM_FORMAT_S16_LE?"SND_PCM_FORMAT_S16_LE":"unknown format" );
     if ((err = snd_pcm_hw_params_set_format (capture_handle, hw_params, format)) < 0) {
-        _prSFn( "cannot set sample format (%s)\n",
-                snd_strerror (err));
+        _prSFn( "cannot set sample format (%d:%s)",
+                err, snd_strerror (err));
         exit (1);
     }
-    _prSFn( "hw_params format setted\n");
+    _prSFn( "hw_params format setted");
 
+    _prSFn( "trying to set hw_params rate to %d" , rate);
     if ((err = snd_pcm_hw_params_set_rate_near (capture_handle, hw_params, &rate, 0)) < 0) {
-        _prSFn( "cannot set sample rate (%s)\n",
-                snd_strerror (err));
+        _prSFn( "cannot set sample rate (%d:%s)",
+                err, snd_strerror (err));
         exit (1);
     }
-    _prSFn( "hw_params rate setted\n");
+    _prSFn( "hw_params rate setted");
 
-    if ((err = snd_pcm_hw_params_set_channels (capture_handle, hw_params, 2)) < 0) {
-        _prSFn( "cannot set channel count (%s)\n",
-                snd_strerror (err));
+    _prSFn( " ----- trying to set channel amount to :%d" , _chAmount ) ;
+    if ((err = snd_pcm_hw_params_set_channels (capture_handle, hw_params, _chAmount )) < 0) {
+        _prSFn( "cannot set channel count (%d:%s)",
+                err, snd_strerror (err));
         exit (1);
     }
-    _prSFn( "hw_params channels setted\n");
+    _prSFn( "hw_params channels setted");
 
     if ((err = snd_pcm_hw_params (capture_handle, hw_params)) < 0) {
-        _prSFn( "cannot set parameters (%s)\n",
-                snd_strerror (err));
+        _prSFn( "cannot set parameters (%d:%s)",
+                err, snd_strerror (err));
         exit (1);
     }
-    _prSFn( "hw_params setted\n");
+    _prSFn( "hw_params setted");
 
     snd_pcm_hw_params_free (hw_params);
-    _prSFn( "hw_params freed\n");
+    _prSFn( "hw_params freed");
 
     if ((err = snd_pcm_prepare (capture_handle)) < 0) {
-        _prSFn( "cannot prepare audio interface for use (%s)\n",
-                snd_strerror (err));
+        _prSFn( "cannot prepare audio interface for use (%d:%s)",
+                err, snd_strerror (err));
         exit (1);
     }
-    _prSFn( "audio interface prepared\n");
+    _prSFn( "audio interface prepared");
 
     buffer = malloc(128 * snd_pcm_format_width(format) / 8 * 2);
-    _prSFn( "buffer allocated\n");
+    _prSFn( "buffer allocated");
 
     for (i = 0; i < 10; ++i) {
         if ((err = snd_pcm_readi (capture_handle, buffer, buffer_frames)) != buffer_frames) {
-            _prSFn( "read from audio interface failed (%d:%s)\n",
+            _prSFn( "read from audio interface failed (%d:%s)",
                     err, snd_strerror (err));
             exit (1);
         }
-        _prSFn( "read %d done\n", i);
+        _prSFn( "read %d done", i);
     }
     free(buffer);
-    _prSFn( "buffer freed\n");
+    _prSFn( "buffer freed");
 
     snd_pcm_close (capture_handle);
-    _prSFn( "audio interface closed\n");
+    _prSFn( "audio interface closed");
 
     exit (0);
 } /* main */
