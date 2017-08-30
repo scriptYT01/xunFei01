ae5a17e95259688255c55194904b1a9aaaff6d9f
yt01 <yt01@jjj123.com>
2017-08-30 16:51:18 +0800

    _2017_0830__1651pm

--- srcXF10/patchXF/sendResultToSdoutOrTCP.cpp
+++ srcXF10/patchXF/sendResultToSdoutOrTCP.cpp
@@ -1,10 +1,17 @@
 #include "sendResultToSdoutOrTCP.h"
 
-_sendResultToSdoutOrTCP::_sendResultToSdoutOrTCP( strring ___outDST )
-    : _outDST( ___outDST ) 
+_sendResultToSdoutOrTCP::_sendResultToSdoutOrTCP( string ___outDST )
+    : 
+        _outDST( ___outDST ) 
 {
 } // _sendResultToSdoutOrTCP::_sendResultToSdoutOrTCP
 
 _sendResultToSdoutOrTCP::~_sendResultToSdoutOrTCP( )
 {
 } // _sendResultToSdoutOrTCP::~_sendResultToSdoutOrTCP
+
+class _sendResultToSdoutOrTCP * _sendResultToSdoutOrTCP::_getOBJ() 
+{
+    //return this;
+    return  _nowOBJ ;
+} // _sendResultToSdoutOrTCP::_getOBJ
--- srcXF10/patchXF/sendResultToSdoutOrTCP.h
+++ srcXF10/patchXF/sendResultToSdoutOrTCP.h
@@ -2,17 +2,22 @@
 #ifndef     __SENDRESULTTOSDOUTORTCP_H
 #define     __SENDRESULTTOSDOUTORTCP_H
 
+#include <string>
+
 using namespace std;
+
 
 class _sendResultToSdoutOrTCP
 {
     private :
         string _outDST ;
+        class _sendResultToSdoutOrTCP * _nowOBJ = NULL ;
     public :
         _sendResultToSdoutOrTCP( string ___outDST ) ;
         ~_sendResultToSdoutOrTCP();
+        class _sendResultToSdoutOrTCP * _getOBJ() ;
 
-} // class _sendResultToSdoutOrTCP
+} ; // class _sendResultToSdoutOrTCP
 
 
 #endif //     __SENDRESULTTOSDOUTORTCP_H
