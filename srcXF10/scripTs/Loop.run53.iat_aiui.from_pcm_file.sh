08c9822b6e7400419d4db163137a7f40fbe24a2b
yt01 <yt01@jjj123.com>
2017-09-05 17:11:28 +0800

    _2017_0905__1711pm

--- srcXF10/scripTs/Loop.run53.iat_aiui.from_pcm_file.sh
+++ srcXF10/scripTs/Loop.run53.iat_aiui.from_pcm_file.sh
@@ -6,12 +6,16 @@
 export PCM00=/ch/11/20170904_16000hz_S16LE_1channel.pcm.person
 export PCM01=${PCM00}.${1}.pcm 
 
-if [ ! -f ${PCM01} ]
+if [ -z "$2" -a ! -f ${PCM01} ]
 then
     echo ; echo "file ${PCM01} don't exist. exit."
     echo
     exit
 fi
+
+## if $2 is not NULL : to specifiy dstIP 
+dstIP=$2
+[ -z "$dstIP" ] && dstIP=127.0.0.1
 
 while [ 1 ]
 do
@@ -22,13 +26,27 @@ do
     #echo    "arecord -D hw:0,2 -f S16_LE -r 16000 -c 1 | ./aiui_sample32.x1000.bin - - | nc110.x1000.bin -w 3 127.0.0.1 33221"
     #echo
     #echo;    arecord -D hw:0,2 -f S16_LE -r 16000 -c 1 | ./aiui_sample32.x1000.bin - - | nc110.x1000.bin -w 3 127.0.0.1 33221
-    echo
-    echo    "cat ${PCM01} | ./aiui_sample32.x1000.bin - - | nc110.x1000.bin -w 3 127.0.0.1 33221"
-    echo
-    echo ;   cat ${PCM01} | ./aiui_sample32.x1000.bin - - | nc110.x1000.bin -w 3 127.0.0.1 33221 
+
+    ## if $2 is not NULL : to specifiy dstIP , connect to the dstIP and get the PCM 
+    if [ -z "$2" ]
+    then
+        echo
+        echo    "cat ${PCM01} | ./aiui_sample32.x1000.bin - - | nc110.x1000.bin -w 3     ${dstIP}     33221"
+        echo
+        echo ;   cat ${PCM01} | ./aiui_sample32.x1000.bin - - | nc110.x1000.bin -w 3     ${dstIP}     33221 
+    else
+        echo
+        echo    "nc110.x1000.bin -w 3 ${dstIP} 33225 | ./aiui_sample32.x1000.bin - - | nc110.x1000.bin -w 3     ${dstIP}     33221"
+        echo
+        echo ;   nc110.x1000.bin -w 3 ${dstIP} 33225 | ./aiui_sample32.x1000.bin - - | nc110.x1000.bin -w 3     ${dstIP}     33221 
+    fi
 
     break ;
     [ 1 == "$1" ] && exit
     sleep 4
+
+    ## if $2 is not NULL : to specifiy dstIP , so exit
+    [ -n "$2" ] && exit
+    
 done
 
