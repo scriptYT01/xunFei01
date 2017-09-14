3fcbbed72450a2a98c7db1db5756fba07821d5ab
yt01 <yt01@jjj123.com>
2017-09-14 18:55:51 +0800

    _2017_0914__1855pm

---
+++ kpvoice01/include/kp___base.h
@@ -0,0 +1,11 @@
+#ifndef     __KP___BASE_H
+#define     __KP___BASE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <errno.h>
+#include <stdarg.h>
+
+
+#endif
---
+++ kpvoice01/src/kp01_main.cpp
@@ -0,0 +1,6 @@
+#include "kp___base.h"
+
+int main( int ___argc , char ** ___argv ) {
+    return 0 ;
+} /* main */
+
