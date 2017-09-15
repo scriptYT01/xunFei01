f7e3b1ef49a2e196a1c97d093ea294fd49b0297e
yt01 <yt01@jjj123.com>
2017-09-15 10:11:59 +0800

    _2017_0915__1011am

--- kpvoice01/src/kp01_main.cpp
+++ kpvoice01/src/kp01_main.cpp
@@ -1,5 +1,10 @@
 #include "kp___base.h"
 
+#define     _kpListenIP     "127.0.0.1"
+#define     _kpTcp_rawpcm   47811
+#define     _kpTcp_filtered 47813
+#define     _kpTcp_speaker  47815
+
 int main( int ___argc , char ** ___argv ) {
     return 0 ;
 } /* main */
