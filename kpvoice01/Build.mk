a470e40785a843f3f45172f4edb6c95e0498352b
yt01 <yt01@jjj123.com>
2017-09-15 16:50:41 +0800

    _2017_0915__1650pm

--- kpvoice01/Build.mk
+++ kpvoice01/Build.mk
@@ -5,15 +5,12 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := kp01.x1000.bin
 LOCAL_MODULE_TAGS := kp01
 LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/ch/kp01
-LOCAL_SRC_FILES := \
+LOCAL_SRC_FILES :=                                \
+	base01/base01_01.c                            \
+	ssuperStream01/superStreamTop.cpp             \
+	ssuperStream01/superStreamOpenOrReopen.cpp    \
 	src/kp01_main.cpp 
 
-#LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc -lrt -ldl -lpthread -lm -lc
-#LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc -lc
-#LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc -lc -lpthread
-#LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc -lc -lpthread -lstdc++
-#LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc -laiui -lc -lpthread -lstdc++
-#LOCAL_LDLIBS := -L$(LOCAL_PATH)/libs/mipsX1000 -lmsc -laiui -lc -lpthread -lstdc++ -ldl
 LOCAL_LDLIBS := -lc -lpthread -lstdc++ -ldl
 LOCAL_CFLAGS := -I$(LOCAL_PATH)/base01
 LOCAL_CFLAGS += -I$(LOCAL_PATH)/base01 -I$(LOCAL_PATH)/ssuperStream01 
