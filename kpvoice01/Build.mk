a07269cc4e65ad4b7e29e6c0e8525eec5b7e4670
yt01 <yt01@jjj123.com>
2017-09-16 16:29:35 +0800

    _2017_0916__1629pm

--- kpvoice01/Build.mk
+++ kpvoice01/Build.mk
@@ -5,11 +5,23 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := kp01.x1000.bin
 LOCAL_MODULE_TAGS := kp01
 LOCAL_MODULE_PATH:=$(TARGET_FS_BUILD)/ch/kp01
-LOCAL_SRC_FILES :=                                \
-	base01/base01_01.c                            \
-	ssuperStream01/superStreamTop.cpp             \
-	ssuperStream01/superStreamOpenOrReopen.cpp    \
-	src/kp01_main.cpp 
+LOCAL_SRC_FILES :=                                				\
+	base01/base01_01.c                            				\
+    ssuperStream01/streamControllerTop.cpp            \
+    ssuperStream01/superStreamBuf.cpp                 \
+    ssuperStream01/superStreamCheck.cpp               \
+    ssuperStream01/superStreamChilds.cpp              \
+    ssuperStream01/superStreamConf.cpp                \
+    ssuperStream01/superStreamDump.cpp                \
+    ssuperStream01/superStreamInit.cpp                \
+    ssuperStream01/superStreamOpenOrReopen.cpp        \
+    ssuperStream01/superStreamRW.cpp                  \
+    ssuperStream01/superStreamStatus.cpp              \
+    ssuperStream01/superStreamTop.cpp                 \
+	src/kp01_loop.cpp 							      \
+	src/kp01_main.cpp 							      \
+	src/kp01_test.cpp 							      \
+
 
 LOCAL_LDLIBS := -lc -lpthread -lstdc++ -ldl
 LOCAL_CFLAGS := -I$(LOCAL_PATH)/base01
