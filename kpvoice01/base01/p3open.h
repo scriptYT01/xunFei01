ee329dcecbb1ec47d50739c1a42261acea21f240
yt01 <yt01@jjj123.com>
2017-10-08 15:12:38 +0800

    _2017_1008__1512pm

--- kpvoice01/base01/p3open.h
+++ kpvoice01/base01/p3open.h
@@ -3,5 +3,21 @@
 
 #include    "base01.h"
 
+class _p3open 
+{
+    public :
+        _p3open( const char * ___cmd ) ;
+        ~_p3open( ){} ;
+    public :
+        bool                        _p3Running( bool  ___exitWhenChildFinished ) ;
+        bool                        _p3CanRead() ;
+        bool                        _p3CanWrite() ;
+    public :
+        int                         _p3WriteNonblock(  int ___len , const char * ___buf ) ;
+        int                         _p3WriteBlock(     int ___len , const char * ___buf ) ;
+        int                         _p3ReadNonblock(   int ___len ,       char * ___buf ) ;
+        int                         _p3ReadBlock(      int ___len ,       char * ___buf ) ;
+    private :
+} ; /* class _p3open */
 
 #endif
