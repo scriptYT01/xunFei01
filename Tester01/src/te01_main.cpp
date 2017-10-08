e76c5640644bccab8afbcb96a55506a277dec490
yt01 <yt01@jjj123.com>
2017-10-08 11:20:08 +0800

    /hom_2017_1008__1120am

--- Tester01/src/te01_main.cpp
+++ Tester01/src/te01_main.cpp
@@ -5,6 +5,8 @@ void _usage( int ___argc , char ** ___argv ) {
 
 void _paraAnalyzeMM( int ___argc , char ** ___argv , uint16_t ___exitPort  ) {
     _paraAnalyzeSS( ___argc , ___argv , 60001) ;
+    
+
     //if ( ___argc > 2 ) { _FrawPcm           =  ___argv[1] ; }
     //_fGenRawPcm    = new _ssFileIn(  _enSsdIn    , _FrawPcm     , " _fGenRawPcm    : rawPCM    generater " );
 } /* _paraAnalyzeMM */
@@ -17,6 +19,10 @@ void _initListen(void) {
 int main( int ___argc , char ** ___argv ) {
 
     int __i01 ;
+    // /testsuite/network/miio_wifi     /dev/ttyS1
+    // /usr/bin/pvalg_ymhood            /dev/snd/pcmC0D2c
+    // /usr/bin/aplay                   /dev/snd/pcmC0D0p
+    // X1000 : pcmC0D0c   pcmC0D0p   pcmC0D1c   pcmC0D1p   pcmC0D2c
 
     _usage( ___argc , ___argv ) ;
 
