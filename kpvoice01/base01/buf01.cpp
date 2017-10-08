5d2d0def9dcdc7547df58fe73025d539b88239f6
yt01 <yt01@jjj123.com>
2017-10-08 18:04:52 +0800

    _2017_1008__1804pm

--- kpvoice01/base01/buf01.cpp
+++ kpvoice01/base01/buf01.cpp
@@ -4,19 +4,26 @@ _Buf * _Buf::_new( _superStreamBase * ___ss , uint16_t ___bfSize , uint16_t ___b
     _Buf * __ss ;
     __ss = new _Buf( ___ss , ___bfSize , ___bfDepth ) ;
 
+    if ( __ss -> _ok() ) {
+        return __ss ;
+    }
+
+    delete __ss ;
     return NULL ;
 } /* _Buf::_new */
 
 _Buf::_Buf( _superStreamBase * ___ss , uint16_t ___bfSize , uint16_t ___bfDepth ) {
     if ( _buf ) { 
         free( _buf ) ; 
+        _buf    = NULL ;
     }
 
     if ( ___ss && ___bfSize && ___bfDepth ) {
         _buf    = (char*) malloc( ___bfSize * ___bfDepth ) ;
+    }
+    if ( _buf ) {
         _bfSS   = ___ss ;
     } else {
-        _buf    = NULL ;
         _bfSS   = NULL ;
     }
     //return NULL ;
