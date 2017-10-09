73ec5ca1d45623ee308c3407101f11c96c381924
yt01 <yt01@jjj123.com>
2017-10-09 13:55:47 +0800

    _2017_1009__1355pm

--- Tester01/te21_target_board.sh
+++ Tester01/te21_target_board.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-init() {
-    lsof  |grep snd |xargs -n 1 kill -9             &> /dev/null
-    lsof  |grep ttyS1 |xargs -n 1 kill -9           &> /dev/null
+initTTY() {
+    lsof  |grep snd     |awk '{print $1}' |xargs -n 1 kill -9           &> /dev/null
+    lsof  |grep ttyS1   |awk '{print $1}' |xargs -n 1 kill -9           &> /dev/null
+
     killall -9 miio_wifi                            &> /dev/null
     killall -9 pvalg_ymhood                         &> /dev/null
     stty -F /dev/ttyS1 -hupcl ignbrk -onlcr -echoe -echok -echoctl -echoke min 1 time 5
@@ -10,6 +11,7 @@ init() {
 
 
 
+initTTY
 
 
 cat /dev/ttyS1
--- Tester01/te22_tester_baord.sh
+++ Tester01/te22_tester_baord.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-init() {
-    lsof  |grep snd |xargs -n 1 kill -9             &> /dev/null
-    lsof  |grep ttyS1 |xargs -n 1 kill -9           &> /dev/null
+initTTY() {
+    lsof  |grep snd     |awk '{print $1}' |xargs -n 1 kill -9           &> /dev/null
+    lsof  |grep ttyS1   |awk '{print $1}' |xargs -n 1 kill -9           &> /dev/null
+
     killall -9 miio_wifi                            &> /dev/null
     killall -9 pvalg_ymhood                         &> /dev/null
     stty -F /dev/ttyS1 -hupcl ignbrk -onlcr -echoe -echok -echoctl -echoke min 1 time 5
@@ -10,7 +11,17 @@ init() {
 
 
 
+initTTY
 
 
-cat /dev/ttyS1
+while read -r line1 ; do
+    #echo "Text read from file: <${line1}>"
+    line2=$(echo -n ${line1}|tr -d '\n\r')
+    wc1=$(echo -n ${line2}|wc -w)
+    if [ 2 = "${wc1}" ]
+    then
+        echo "line2 <${line2}>"
+    fi
+done < /dev/ttyS1
+
 
