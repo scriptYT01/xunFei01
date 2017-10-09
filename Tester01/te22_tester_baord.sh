5c4ff24429f733a4935f08e11748a9cc91d59409
yt01 <yt01@jjj123.com>
2017-10-09 14:14:21 +0800

    _2017_1009__1414pm

--- Tester01/te22_tester_baord.sh
+++ Tester01/te22_tester_baord.sh
@@ -14,13 +14,30 @@ initTTY() {
 initTTY
 
 
+hh1=/vt/VIOMI_test_wav/M2CHN02VM_AAQ0
+hh2=.wav
+
 while read -r line1 ; do
     #echo "Text read from file: <${line1}>"
     line2=$(echo -n ${line1}|tr -d '\n\r')
     wc1=$(echo -n ${line2}|wc -w)
     if [ 2 = "${wc1}" ]
     then
-        echo "line2 <${line2}>"
+        back1="play failed1" 
+        word1=$(echo -n ${line2}|awk '{print $1}')
+        word2=$(echo -n ${line2}|awk '{print $2}')
+        if [ 'play' = "${word1}" ]
+        then
+            wav1="${hh1}${word2}${hh2}"
+            #echo "wav1 1: <${wav1}>"
+            if [ -f "${wav1}" ]
+            then
+                echo "wav1 2: <${wav1}>"
+                aplay -f S16_LE -r 16000 "${wav1}"
+                back1="play ok1" 
+            fi
+            echo ${back1} "${wav1}" > /dev/ttyS1
+        fi
     fi
 done < /dev/ttyS1
 
