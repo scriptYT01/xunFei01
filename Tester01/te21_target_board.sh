b37ad8998d0e63c2b78330f91a9e15e2c6f5dec3
yt01 <yt01@jjj123.com>
2017-10-09 10:22:15 +0800

    _2017_1009__1022am

--- Tester01/te21_target_board.sh
+++ Tester01/te21_target_board.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
+init() {
+    lsof  |grep snd |xargs -n 1 kill -9             &> /dev/null
+    lsof  |grep ttyS1 |xargs -n 1 kill -9           &> /dev/null
+    killall -9 miio_wifi                            &> /dev/null
+    killall -9 pvalg_ymhood                         &> /dev/null
+    stty -F /dev/ttyS1 -hupcl ignbrk -onlcr -echoe -echok -echoctl -echoke min 1 time 5
+}
+
+
+
+
+
+cat /dev/ttyS1
 
--- Tester01/te22_tester_baord.sh
+++ Tester01/te22_tester_baord.sh
@@ -5,6 +5,7 @@ init() {
     lsof  |grep ttyS1 |xargs -n 1 kill -9           &> /dev/null
     killall -9 miio_wifi                            &> /dev/null
     killall -9 pvalg_ymhood                         &> /dev/null
+    stty -F /dev/ttyS1 -hupcl ignbrk -onlcr -echoe -echok -echoctl -echoke min 1 time 5
 }
 
 
