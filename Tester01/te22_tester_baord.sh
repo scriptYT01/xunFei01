3ab4d7b5b31e4fc4763fd12d3eedd7fcccb8c6c0
yt01 <yt01@jjj123.com>
2017-10-09 10:09:13 +0800

    _2017_1009__1009am

--- Tester01/te22_tester_baord.sh
+++ Tester01/te22_tester_baord.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
+init() {
+    lsof  |grep snd |xargs -n 1 kill -9             &> /dev/null
+    lsof  |grep ttyS1 |xargs -n 1 kill -9           &> /dev/null
+    killall -9 miio_wifi                            &> /dev/null
+    killall -9 pvalg_ymhood                         &> /dev/null
+}
+
+
+
+
+
+cat /dev/ttyS1
 
